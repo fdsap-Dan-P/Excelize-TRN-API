3aa18c755ceaf7f2b513dbbc16b6cf059eb09aeb
fdsap-Dan <roldan.polintang@fortress-asya.com>
2023-03-23 14:56:56 +0800

    add login request struct

--- struct/request/CredentialsRequest.go
+++ struct/request/CredentialsRequest.go
@@ -1,7 +1,13 @@
 package request
 
 type RegisteredRequest struct {
+	Name            string `json:"name"`
 	Username        string `json:"username"`
 	Password        string `json:"password"`
 	Retype_password string `json:"retype_password"`
+}
+
+type LogInRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
